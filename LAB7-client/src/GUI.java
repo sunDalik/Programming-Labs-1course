dad856cd45ff46b8243fafdcf0291c762c5b8484
sunDalik <dali1999@mail.ru>
2018-10-13 22:42:09 +0300

    perfect layout

--- LAB7-client/src/GUI.java
+++ LAB7-client/src/GUI.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.messaging.saaj.soap.JpegDataContentHandler;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
@@ -7,7 +9,7 @@ class GUI extends JFrame{
     GUI(){
 
         //Top panel elements
-        JLabel hoText = new JLabel("Highlighted Object - ");
+        JLabel hoText = new JLabel(" Highlighted Object - ");
         JLabel nameText = new JLabel("Name:");
         JLabel sizeText = new JLabel("Size:");
         JLabel powerText = new JLabel("Power:");
@@ -15,20 +17,20 @@ class GUI extends JFrame{
         JLabel yText = new JLabel("Y:");
         JLabel colorText = new JLabel("Color:");
         JLabel dateText = new JLabel("Creation Date:");
-        JLabel nameValue = new JLabel();
-        JLabel sizeValue = new JLabel();
-        JLabel powerValue = new JLabel();
-        JLabel xValue = new JLabel();
-        JLabel yValue = new JLabel();
-        JLabel colorValue = new JLabel();
-        JLabel dateValue = new JLabel();
-        nameValue.setBorder(new LineBorder(Color.BLACK, 1));
-        sizeValue.setBorder(new LineBorder(Color.BLACK, 1));
-        powerValue.setBorder(new LineBorder(Color.BLACK, 1));
-        xValue.setBorder(new LineBorder(Color.BLACK, 1));
-        yValue.setBorder(new LineBorder(Color.BLACK, 1));
-        colorValue.setBorder(new LineBorder(Color.BLACK, 1));
-        dateValue.setBorder(new LineBorder(Color.BLACK, 1));
+        JTextField nameValue = new JTextField();
+        JTextField sizeValue = new JTextField();
+        JTextField powerValue = new JTextField();
+        JTextField xValue = new JTextField();
+        JTextField yValue = new JTextField();
+        JTextField colorValue = new JTextField();
+        JTextField dateValue = new JTextField();
+        nameValue.setEditable(false);
+        sizeValue.setEditable(false);
+        powerValue.setEditable(false);
+        xValue.setEditable(false);
+        yValue.setEditable(false);
+        colorValue.setEditable(false);
+        dateValue.setEditable(false);
 
         //Right panel elements
         JLabel connectionText = new JLabel();
@@ -81,25 +83,33 @@ class GUI extends JFrame{
         yToSlider.setMaximum(1000);
         JLabel yTo = new JLabel("0");
         yToSlider.addChangeListener(e -> yTo.setText(Integer.toString(yToSlider.getValue())));
+        xFrom.setPreferredSize(new Dimension(30,20));
+        xTo.setPreferredSize(new Dimension(30,20));
+        yFrom.setPreferredSize(new Dimension(30,20));
+        yTo.setPreferredSize(new Dimension(30,20));
         JButton startButton = new JButton("Start");
         JButton stopButton = new JButton("Stop");
 
         //Panels
         JPanel p17 = new JPanel();
-        p17.setLayout(new BoxLayout(p17, BoxLayout.X_AXIS));
+        p17.setLayout(new GridLayout(1,2));
         p17.add(connectionText);
-        p17.add(refreshButton);
+        JPanel refreshButtonPanel = new JPanel();
+        refreshButtonPanel.setLayout(new BoxLayout(refreshButtonPanel, BoxLayout.X_AXIS));
+        refreshButtonPanel.add(Box.createRigidArea(new Dimension(50,0)));
+        refreshButtonPanel.add(refreshButton);
+        p17.add(refreshButtonPanel);
 
         JPanel p16 = new JPanel();
         p16.setLayout(new BorderLayout());
-        p16.add(filtersText, BorderLayout.NORTH);
+        p16.add(filtersText);
 
         JPanel p15 = new JPanel();
         p15.setLayout(new BoxLayout(p15,BoxLayout.X_AXIS));
         p15.add(colorText2);
 
         JPanel p14 = new JPanel();
-        p14.setLayout(new GridLayout(3,2,20,10));
+        p14.setLayout(new GridLayout(3,2,0,5));
         p14.add(blueCheckBox);
         p14.add(cyanCheckBox);
         p14.add(sapphireCheckBox);
@@ -107,27 +117,39 @@ class GUI extends JFrame{
         p14.add(navyCheckBox);
         p14.add(emeraldCheckBox);
 
+        JPanel p14extended = new JPanel();
+        p14extended.setLayout(new BoxLayout(p14extended, BoxLayout.X_AXIS));
+        p14extended.add(Box.createRigidArea(new Dimension(60,0)));
+        p14extended.add(p14);
+
         JPanel p13 = new JPanel();
         p13.setLayout(new BoxLayout(p13,BoxLayout.X_AXIS));
         p13.add(nameStarts);
+        p13.add(Box.createRigidArea(new Dimension(10,0)));
         p13.add(nameField);
 
         JPanel p12 = new JPanel();
         p12.setLayout(new BoxLayout(p12,BoxLayout.X_AXIS));
         p12.add(sizeFrom);
+        p12.add(Box.createRigidArea(new Dimension(7,0)));
         p12.add(sizeFromTo);
+        p12.add(Box.createRigidArea(new Dimension(7,0)));
         p12.add(sizeTo);
 
         JPanel p11 = new JPanel();
         p11.setLayout(new BoxLayout(p11,BoxLayout.X_AXIS));
         p11.add(powerFrom);
+        p11.add(Box.createRigidArea(new Dimension(7,0)));
         p11.add(powerFromTo);
+        p11.add(Box.createRigidArea(new Dimension(7,0)));
         p11.add(powerTo);
 
         JPanel p10 = new JPanel();
         p10.setLayout(new BoxLayout(p10,BoxLayout.X_AXIS));
         p10.add(dateFrom);
+        p10.add(Box.createRigidArea(new Dimension(7,0)));
         p10.add(dateFromTo);
+        p10.add(Box.createRigidArea(new Dimension(7,0)));
         p10.add(dateTo);
 
         JPanel p9 = new JPanel();
@@ -137,6 +159,7 @@ class GUI extends JFrame{
         JPanel p8 = new JPanel();
         p8.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(0,10,10,10);
         c.gridx = 0;
         c.gridy = 0;
         p8.add(new JLabel("from:"), c);
@@ -146,11 +169,11 @@ class GUI extends JFrame{
         p8.add(xFrom,c);
         c.gridx = 0;
         c.gridy++;
-        p9.add(new JLabel("to:"),c);
+        p8.add(new JLabel("to:"),c);
         c.gridx++;
-        p9.add(xToSlider,c);
+        p8.add(xToSlider,c);
         c.gridx++;
-        p9.add(xTo,c);
+        p8.add(xTo,c);
 
         JPanel p7 = new JPanel();
         p7.setLayout(new BoxLayout(p7,BoxLayout.X_AXIS));
@@ -159,76 +182,128 @@ class GUI extends JFrame{
         JPanel p6 = new JPanel();
         p6.setLayout(new GridBagLayout());
         GridBagConstraints c2 = new GridBagConstraints();
+        c2.insets = new Insets(0,10,10,10);
         c2.gridx = 0;
         c2.gridy = 0;
-        p6.add(new JLabel("from:"));
+        p6.add(new JLabel("from:"),c2);
         c2.gridx++;
-        p6.add(yFromSlider);
+        p6.add(yFromSlider,c2);
         c2.gridx++;
-        p6.add(yFrom);
+        p6.add(yFrom,c2);
         c2.gridx = 0;
         c2.gridy++;
-        p6.add(new JLabel("to:"));
+        p6.add(new JLabel("to:"),c2);
         c2.gridx++;
-        p6.add(yToSlider);
+        p6.add(yToSlider,c2);
         c2.gridx++;
-        p6.add(yTo);
+        p6.add(yTo,c2);
 
         JPanel p5 = new JPanel();
         p5.setLayout(new BoxLayout(p5,BoxLayout.X_AXIS));
         p5.add(startButton);
-        p5.add(Box.createRigidArea(new Dimension(20,0)));
+        p5.add(Box.createRigidArea(new Dimension(30,0)));
         p5.add(stopButton);
 
         JPanel p4 = new JPanel();
         p4.setLayout(new BoxLayout(p4,BoxLayout.Y_AXIS));
-        p4.add(p17);
-        p4.add(p16);
-        p4.add(p15);
-        p4.add(p14);
-        p4.add(p13);
-        p4.add(p12);
-        p4.add(p11);
-        p4.add(p10);
-        p4.add(p9);
         p4.add(Box.createRigidArea(new Dimension(0,10)));
+        p4.add(p17);
+        p4.add(Box.createRigidArea(new Dimension(0,10)));
+        p4.add(p16);
+        p4.add(Box.createRigidArea(new Dimension(0,10)));
+        p4.add(p15);
+        p4.add(Box.createRigidArea(new Dimension(0,10)));
+        p4.add(p14extended);
+        p4.add(Box.createRigidArea(new Dimension(0,15)));
+        p4.add(p13);
+        p4.add(Box.createRigidArea(new Dimension(0,15)));
+        p4.add(p12);
+        p4.add(Box.createRigidArea(new Dimension(0,15)));
+        p4.add(p11);
+        p4.add(Box.createRigidArea(new Dimension(0,15)));
+        p4.add(p10);
+        p4.add(Box.createRigidArea(new Dimension(0,15)));
+        p4.add(p9);
         p4.add(p8);
+        p4.add(Box.createRigidArea(new Dimension(0,10)));
         p4.add(p7);
         p4.add(p6);
+        p4.add(Box.createRigidArea(new Dimension(0,10)));
         p4.add(p5);
+        p4.add(Box.createRigidArea(new Dimension(0,15)));
+
+        JPanel p4extended = new JPanel();
+        p4extended.setLayout(new BoxLayout(p4extended, BoxLayout.X_AXIS));
+        p4extended.add(Box.createRigidArea(new Dimension(10,0)));
+        p4extended.add(p4);
+        p4extended.add(Box.createRigidArea(new Dimension(10,0)));
 
         JPanel p3 = new JPanel();
         p3.setLayout(new BorderLayout());
 
         JPanel p2 = new JPanel();
         p2.setLayout(new BoxLayout(p2, BoxLayout.X_AXIS));
+        hoText.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        nameText.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        sizeText.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        powerText.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        xText.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        yText.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        colorText.setFont(new Font("Helvetica", Font.PLAIN, 16));
+        dateText.setFont(new Font("Helvetica", Font.PLAIN, 16));
         p2.add(hoText);
         p2.add(nameText);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(nameValue);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(sizeText);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(sizeValue);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(powerText);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(powerValue);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(xText);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(xValue);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(yText);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(yValue);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(colorText);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(colorValue);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(dateText);
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
         p2.add(dateValue);
-        p2.setBorder(new LineBorder(Color.BLACK, 2));
+        p2.add(Box.createRigidArea(new Dimension(10,0)));
+
+        JPanel p2extended = new JPanel();
+        p2extended.setLayout(new BoxLayout(p2extended, BoxLayout.Y_AXIS));
+        JPanel topSpace = new JPanel();
+        topSpace.setLayout(new BorderLayout());
+        topSpace.add(Box.createRigidArea(new Dimension(0,8)), BorderLayout.NORTH);
+        JPanel botSpace = new JPanel();
+        botSpace.setLayout(new BorderLayout());
+        botSpace.add(Box.createRigidArea(new Dimension(0,8)), BorderLayout.NORTH);
+        p2extended.add(topSpace);
+        p2extended.add(p2);
+        p2extended.add(botSpace);
+        botSpace.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
 
         JPanel p1 = new JPanel();
         p1.setLayout(new BorderLayout());
-        p1.add(p2, BorderLayout.NORTH);
+        p1.add(p2extended, BorderLayout.NORTH);
         p1.add(p3, BorderLayout.CENTER);
-        p1.add(p4, BorderLayout.EAST);
+        p1.add(p4extended, BorderLayout.EAST);
         add(p1);
 
 
         setTitle("Sea Collection Viewer");
-        setSize(1200,900);
+        setSize(1000,700);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
